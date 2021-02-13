e79788c82719dc635fdfb6927b0b6f00ee4e3436
Harry Maclean <harry@hmac.dev>
2021-02-13 11:06:16 +0000

    Update nixpkgs and GHC

--- default.nix
+++ default.nix
@@ -1,26 +1,27 @@
 let
   name = "kite";
-  compiler = "ghc883";
+  compiler = "ghc8102";
 
-  sources = import ./nix/sources.nix {};
-  nixpkgs = import sources.nixpkgs {};
+  sources = import ./nix/sources.nix { };
+  nixpkgs = import sources.nixpkgs { };
   gitignore = nixpkgs.nix-gitignore.gitignoreSourcePure [ ./.gitignore ];
 
   haskellPackages = nixpkgs.pkgs.haskell.packages.${compiler}.override {
     overrides = self: super: {
-      "${name}" = self.callCabal2nix "${name}" (gitignore ./.) {};
+      "${name}" = self.callCabal2nix "${name}" (gitignore ./.) { };
       # hspec-hedgehog = self.callHackage "hspec-hedgehog" "0.0.1.2" {};
     };
   };
 
   shell = haskellPackages.shellFor {
-    packages = p: [p."${name}"];
+    packages = p: [ p."${name}" ];
     buildInputs = with nixpkgs.haskellPackages; [
       haskellPackages.cabal-install
       ghcid
       brittany
       hlint
       nixpkgs.nixpkgs-fmt
+      nixpkgs.chez
     ];
     withHoogle = true;
   };
