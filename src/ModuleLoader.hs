2973815dcc9965d6907f09ac7f0ac817c2fdadfd
Harry Maclean <harry@hmac.dev>
2021-05-18 19:41:42 +0100

    Don't load the same module twice
    This cuts the time spent parsing by at least half, at least for
    ListTest.

--- src/ModuleLoader.hs
+++ src/ModuleLoader.hs
@@ -6,6 +6,13 @@ module ModuleLoader
   -- , loadFromPathAndRootDirectory
   ) where
 
+import           Data.IORef                     ( IORef
+                                                , atomicModifyIORef'
+                                                , newIORef
+                                                , readIORef
+                                                )
+import           Data.Map.Strict                ( Map )
+import qualified Data.Map.Strict               as Map
 import           System.Directory               ( getCurrentDirectory )
 
 import           AST                            ( Expr )
@@ -40,6 +47,9 @@ import           Util
 -- Similarly, if Data.Map depends on any modules we need to do the same for it.
 -- For now, we ignore things like aliases, qualification and import lists.
 
+-- We use a global cache of parsed modules to prevent parsing the same file twice.
+type ModuleCache = IORef (Map String (Either String Module))
+
 -- TODO: structured errors
 loadFromPath :: FilePath -> IO (Either String UntypedModuleGroup)
 loadFromPath path = do
@@ -50,10 +60,15 @@ loadFromPathAndRootDirectory
   :: FilePath -> FilePath -> IO (Either String UntypedModuleGroup)
 loadFromPathAndRootDirectory path root = do
   modul <- parseKiteFile path <$> readFile path
+
   case modul of
     Left  err -> pure (Left err)
     Right m   -> do
-      deps <- mapM (loadAll root) (dependencies m)
+      -- Create a mutable map to store the contents of each module as we parse them.
+      -- This stops us parsing the same module multiple times.
+      cache <- newIORef (Map.singleton path modul)
+
+      deps  <- mapM (loadAll root cache) (dependencies m)
       case sequence deps of
         Left  err   -> pure (Left err)
         Right deps' -> case sortModules (nub (concat deps')) of
@@ -71,22 +86,27 @@ loadFromPathAndRootDirectory path root = do
                   <> ", cannot find module "
                   <> showModuleName missingModule
 
-loadAll :: FilePath -> ModuleName -> IO (Either String [Module])
-loadAll root name = do
-  modul <- load root name
+loadAll :: FilePath -> ModuleCache -> ModuleName -> IO (Either String [Module])
+loadAll root cache name = do
+  modul <- load root cache name
   case modul of
     Left  err -> pure (Left err)
     Right m   -> do
-      deps <- mapM (loadAll root) (dependencies m)
+      deps <- mapM (loadAll root cache) (dependencies m)
       case sequence deps of
         Left  err   -> pure (Left err)
         Right deps' -> do
           let m' = expandExports m
           pure $ Right $ m' : concat deps'
 
-load :: FilePath -> ModuleName -> IO (Either String Module)
-load root name =
-  let path = filePath root name in parseKiteFile path <$> readFile path
+load :: FilePath -> ModuleCache -> ModuleName -> IO (Either String Module)
+load root cache name = do
+  let path = filePath root name
+  Map.lookup path <$> readIORef cache >>= \case
+    Just m -> pure m
+    _      -> do
+      m <- parseKiteFile path <$> readFile path
+      atomicModifyIORef' cache $ \c -> (Map.insert path m c, m)
 
 -- We skip any references to Kite.Primitive because it's not a normal module.
 -- It has no corresponding file and its definitions are automatically in scope
