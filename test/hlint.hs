d83e119d3cd957cc79d0227f4b4b8440617d31d1
Harry Maclean <harryjmaclean@googlemail.com>
2021-07-04 07:50:31 +0100

    Run hlint on all source directories
    And work around a parsing bug
    
    https://github.com/ndmitchell/hlint/issues/216

--- test/hlint.hs
+++ test/hlint.hs
@@ -5,8 +5,12 @@ import           Language.Haskell.HLint
 import           System.Environment             ( getArgs )
 import           System.Exit                    ( exitFailure )
 
+-- Run hlint on all source directories.
+-- -XNoPatternSynonyms is to work around a bug in hlint where it can't parse code with
+-- variables called 'pattern' (https://github.com/ndmitchell/hlint/issues/216)
 main :: IO ()
 main = do
   args  <- getArgs
-  hints <- hlint $ ["src", "test"] ++ args
+  hints <-
+    hlint $ ["-XNoPatternSynonyms", "src", "test", "app", "benchmarks"] ++ args
   unless (null hints) exitFailure
