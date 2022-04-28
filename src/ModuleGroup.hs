1c064dbcaf32b255809555ac2ec950fcb0f274b5
Harry Maclean <harry@hmac.dev>
2022-04-28 15:27:46 +1200

    Update code to remove some inline comments
    This gets rid of some warnings from Brittany

--- src/ModuleGroup.hs
+++ src/ModuleGroup.hs
@@ -6,8 +6,10 @@ import           Syn
 import qualified Syn.Typed                     as T
 
 -- | A module group is a module together with its dependencies (other modules).
-data ModuleGroup exp ty = ModuleGroup (Module_ Name exp ty)
-                                      [Module_ Name exp ty]
+data ModuleGroup exp ty = ModuleGroup
+  { rootModule   :: Module_ Name exp ty
+  , dependencies :: [Module_ Name exp ty]
+  }
   deriving Show
 type UntypedModuleGroup = ModuleGroup Can.Exp Can.Type
 data TypedModuleGroup = TypedModuleGroup T.Module [T.Module]
--- src/ModuleLoader.hs
+++ src/ModuleLoader.hs
@@ -111,14 +111,15 @@ loadFromPackageInfo info path = do
       -- This stops us parsing the same module multiple times.
       cache                          <- liftIO $ newIORef (Map.singleton path m)
 
-      deps <- mapM (loadAll info cache) (dependencies m)
+      deps <- mapM (loadAll info cache) (directDependencies m)
       sortedDeps                     <- sortModules (nub (concat deps))
       (expandedModule, expandedDeps) <-
         runExceptT (expandImports (expandExports m) sortedDeps) >>= \case
           Left  err -> throwError $ ImportError err
           Right r   -> pure r
-      pure $ ModuleGroup (canonicaliseModule expandedModule)
-                         (map canonicaliseModule expandedDeps)
+      pure $ ModuleGroup { rootModule   = canonicaliseModule expandedModule
+                         , dependencies = map canonicaliseModule expandedDeps
+                         }
 
 -- TODO: deprecate and remove in favour of 'loadFromPackageInfo'
 loadFromPathAndRootDirectory
@@ -142,7 +143,7 @@ loadAll
   -> m [Module]
 loadAll info cache pkgModuleName = do
   modul <- load info cache pkgModuleName
-  deps  <- mapM (loadAll info cache) (dependencies modul)
+  deps  <- mapM (loadAll info cache) (directDependencies modul)
   let modul' = expandExports modul
   pure $ modul' : concat deps
 
@@ -168,8 +169,9 @@ load info cache name@(PkgModuleName pkgName _) = do
         Right m'  -> do
           liftIO $ atomicModifyIORef' cache $ \c -> (Map.insert path m' c, m')
 
-dependencies :: Module_ n (Expr n ty) ty -> [PkgModuleName]
-dependencies Module { moduleImports = imports } = nub $ map importName imports
+directDependencies :: Module_ n (Expr n ty) ty -> [PkgModuleName]
+directDependencies Module { moduleImports = imports } =
+  nub $ map importName imports
 
 -- | Attempt to construct a file path to the given module.
 -- If the module is in our own package, use the root package directory.
